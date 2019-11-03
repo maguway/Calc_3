efaa81bc9bb2c5b3a11de0bd246e7f061578ae69
Ustas <maguway@gmail.com>
2019-11-03 05:20:28 +0300

    litle fix roman

--- src/com/Calculate.java
+++ src/com/Calculate.java
@@ -119,6 +119,10 @@ public class Calculate {
 
     public static String toRoman (int number) {
         TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
         map.put(10, "X");
         map.put(9, "IX");
         map.put(5, "V");
